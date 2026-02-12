3364e97457b5170953a0bb7fc11ce28fa05e1719
balas-1908 <balaji57671@gmail.com>
2026-02-12 22:20:55 +0530

    Update nav.js

--- src/component/nav.js
+++ src/component/nav.js
@@ -9,9 +9,9 @@ function Nav() {
 
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav me-auto">
-          <li className="nav-item"><a className="nav-link" href="#">Shops</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">Offers</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+          <li className="nav-item"><a className="nav-link" href="/home">Shops</a></li>
+          <li className="nav-item"><a className="nav-link" href="/home">Offers</a></li>
+          <li className="nav-item"><a className="nav-link" href="/home">Contact</a></li>
         </ul>
 
         <Link to="/cart" className="btn btn-success">
